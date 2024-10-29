9bc79b4e8822f4724be972d3365f1425e5390851
Liam Mehle <liammehlex@gmail.com>
2024-10-29 11:32:03 +0100

    working logging to mariadb

--- index.ts
+++ index.ts
@@ -1,6 +1,19 @@
 // ethersSetup.ts
 import { ethers } from 'ethers';
 import nftAbi from './shyTokenAbi.js';
+import mariadb from 'mariadb';
+
+console.log('init');
+const pool = mariadb.createPool({
+    host: 'localhost', 
+    user:'root',
+    password: '1234',
+    connectionLimit: 1,
+    database: 'nft'
+});
+
+console.log('connecting');
+const dbConnection = await pool.getConnection();
 
 // Use the Infura provider
 const provider = new ethers.InfuraProvider('sepolia', process.env.INFURA_API_KEY);
@@ -11,21 +24,32 @@ if (contractAddress === undefined)
 const contract = new ethers.Contract(contractAddress, nftAbi, provider);
 const processedTxs = new Set<string>();
 
-contract.on("event TokenMinted(uint id)", (id, event) => {
+contract.on("event TokenMinted(uint id)", (id, event: ethers.EventLog) => {
     if (processedTxs.has(event.transactionHash))
         return;
     processedTxs.add(event.transactionHash);
     console.log(`Token minted with ID ${id}`);
+    event.getTransactionReceipt().then(receipt => {
+        const to = receipt.from.replace('0x', '');
+        dbConnection.execute(`INSERT INTO \`events\` (eventType, newOwner) VALUES ("TokenMinted", "${to}");`);
+    });
 });
-contract.on("event TokenGiven(address from, address to, uint id)", (from, to, id, event) => {
+contract.on("event TokenGiven(address from, address to, uint id)", (from: string, to: string, id: string, event: ethers.EventLog) => {
     if (processedTxs.has(event.transactionHash))
         return;
     processedTxs.add(event.transactionHash);
     console.log(`Token ${id} given by ${from} to ${to}`);
+    from = from.replace('0x', '');
+    to   =   to.replace('0x', '');
+    dbConnection.execute(`INSERT INTO \`events\` (eventType, token, previousOwner, newOwner) VALUES ("TokenGiven", ${id}, "${from}", "${to}");`);
 });
-contract.on("event TokenDestroyed(uint id)", (id, event) => {
+contract.on("event TokenDestroyed(uint id)", (id, event: ethers.EventLog) => {
     if (processedTxs.has(event.transactionHash))
         return;
     processedTxs.add(event.transactionHash);
     console.log(`Token ${id} destroyed`);
+    event.getTransactionReceipt().then(receipt => {
+        const from = receipt.from.replace('0x', '');
+        dbConnection.execute(`INSERT INTO \`events\` (eventType, token, previousOwner) VALUES ("TokenGiven", ${id}, "${from}");`);
+    });
 });
